913c249d071229cfd64d661caabcea4ad6b4cbc4
kelseydh <kelseyh@gmail.com>
2018-07-09 22:50:52 -0700

    remove n+1 query

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.includes(:product_scrapes).all
   end
 
   # GET /products/1
